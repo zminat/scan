c337a9a81b97d4e4b94d4221aa97d67cdfd19590
Natalia Zmitrichenko <rubanka.ru@gmail.com>
2025-03-11 02:01:16 +0500

    Fixed menu elements.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,32 +5,53 @@ import { NavLink, useNavigate } from "react-router-dom";
 import { useAuth } from "./AuthContext.tsx";
 import LimitInfo from "./LimitInfo.tsx";
 import UserInfo from "./UserInfo.tsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import { slide as Menu } from "react-burger-menu";
 
 function Header() {
     const { isLoggedIn } = useAuth();
     const navigate = useNavigate();
     const [menuOpen, setMenuOpen] = useState(false);
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 1000);
+
+    useEffect(() => {
+        const handleResize = () => setIsMobile(window.innerWidth < 1000);
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
 
     const handleMenuStateChange = (state: { isOpen: boolean | ((prevState: boolean) => boolean); }) => {
         setMenuOpen(state.isOpen);
+    };
+
+    const handleCloseMenu = () => {
+        setMenuOpen(false);
     };
 
     const styles = {
         bmBurgerButton: {
             display: 'none'
             },
-        bmItem: {
-            display: 'inline-block'
-        },
+        // bmItem: {
+        //     display: 'inline-block'
+        // },
         bmMenuWrap: {
             background: 'var(--main-color)',
-            margin: '0 auto',
+            cursor: 'context-menu',
             top: '0',
-            width: '100%'
+            width: '100%',
+            high: 'auto'
+        },
+        bmItemList: {
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            paddingTop: '120px'
+        },
+        bmOverlay: {
+            display: 'none'
         }
-    }
+    };
 
     return (
         <header className="header">
@@ -40,7 +61,7 @@ function Header() {
                 </div>
             ) : (
                 <div className="header-icon">
-                <img src={headerIconUrl} alt="Header Icon"/>
+                    <img src={headerIconUrl} alt="Header Icon"/>
                 </div>
             )}
 
@@ -53,12 +74,12 @@ function Header() {
             <div className="header-actions">
                 {isLoggedIn ? (
                     <>
-                        {!menuOpen ? (<LimitInfo/>) : ("")}
-                        <UserInfo/>
+                        {!menuOpen ? (<LimitInfo />) : null}
+                        {!isMobile ? (<UserInfo/>) : null}
                     </>
                 ) : (
                     <>
-                        {!menuOpen ? (
+                        {!menuOpen && !isMobile ? (
                             <>
                                 <NavLink to="/auth" className="register-link">Зарегистрироваться</NavLink>
                                 <div className="header-divider"></div>
@@ -66,52 +87,40 @@ function Header() {
                                     Войти
                                 </button>
                             </>
-                        ) : ("")}
+                        ) : null}
                     </>
                 )}
             </div>
+
             <div className="burger-container">
-            <Menu styles = {styles} right isOpen={menuOpen} width={ '50%' } itemListElement="div" onStateChange={handleMenuStateChange}>
-                {menuOpen && (
-                    <div className="menu-page">
-                        <div className="menu-nav">
-                            <NavLink to="/" className="burger-bar" onClick={() => setMenuOpen(false)}>Главная</NavLink>
-                            <NavLink to="/" className="burger-bar" onClick={() => setMenuOpen(false)}>Тарифы</NavLink>
-                            <NavLink to="/" className="burger-bar" onClick={() => setMenuOpen(false)}>FAQ</NavLink>
-                        </div>
-                        {isLoggedIn ? (<UserInfo/>) : (
-                            <div className="reg-block">
-                                <NavLink to="/auth" className="register-link">Зарегистрироваться</NavLink>
-                                <button className="login-button" onClick={() => navigate("/auth")}>
-                                    Войти
-                                </button>
+                <div className="burger-container">
+                    <Menu styles={styles} right isOpen={menuOpen} onStateChange={handleMenuStateChange} width={'100%'}>
+                        <div className="menu-page">
+                            <div className="menu-nav">
+                                <NavLink to="/" className="burger-bar" onClick={handleCloseMenu}>Главная</NavLink>
+                                <NavLink to="/" className="burger-bar" onClick={handleCloseMenu}>Тарифы</NavLink>
+                                <NavLink to="/" className="burger-bar" onClick={handleCloseMenu}>FAQ</NavLink>
                             </div>
-                        )}
+
+                            {isLoggedIn ? (
+                                <UserInfo />
+                            ) : (
+                                <div className="reg-block">
+                                    <NavLink to="/auth" className="register-link">Зарегистрироваться</NavLink>
+                                    <button className="login-button" onClick={() => { navigate("/auth"); handleCloseMenu(); }}>
+                                        Войти
+                                    </button>
+                                </div>
+                            )}
+                        </div>
+                    </Menu>
+                    <div className={`icon ${menuOpen ? "active" : ""}`} onClick={() => setMenuOpen(!menuOpen)}>
+                        <div className="hamburger"></div>
                     </div>
-                )}
-                <div>
-                    {isLoggedIn ? (
-                        <>
-                            {!menuOpen ? (<LimitInfo/>) : ("")}
-                            <UserInfo/>
-                        </>
-                        ) : (
-                        <>
-                            <NavLink to="/auth" className="register-link">Зарегистрироваться</NavLink>
-                            <div className="header-divider"></div>
-                            <button className="login-button" onClick={() => navigate("/auth")}>
-                                Войти
-                            </button>
-                        </>
-                )}
                 </div>
-            </Menu>
-            <div className={`icon ${menuOpen ? "active" : ""}`} onClick={() => setMenuOpen(!menuOpen)}>
-                <div className="hamburger"></div>
             </div>
-        </div>
         </header>
-    );
+);
 }
 
 export default Header;
